58541e790e53734cbfe2f4d16ae1bedca75cac98
Julian Valencia <juanval1986@gmail.com>
2021-06-20 01:51:43 -0500

    Avance homepage con tarjetas de ciudades top e info.

---
+++ public/assets/js/weatherchecker.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    const cards = $('.weather-card');
+    const owmUrlCurrentWeather = 'http://api.openweathermap.org/data/2.5/weather';
+    const owmApiKey = 'bb085da98830aaad2383873d152174f9';
+    cards.each(function (idx, elm) {
+        let card = $(elm);
+        let data = card.data();
+        $.get(
+            owmUrlCurrentWeather,
+            {zip: data.zipcode, appid: owmApiKey, units: 'metric'},
+            function (data,textStatus,jqXHR) {
+                card.find('#temp').append(data.main.temp);
+                card.find('#humidity').append(data.main.humidity);
+            }
+        );
+    });
+});
